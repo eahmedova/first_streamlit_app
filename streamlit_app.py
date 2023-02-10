87f9cedda5502459e51418bf5175ad34bce24eb2
Elmira Ahmedova <88899832+eahmedova@users.noreply.github.com>
2023-02-10 11:59:24 +0100

    removing response code

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response)
 
 # take the kson version of API response and normalize it
