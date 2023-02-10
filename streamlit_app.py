2ea1410d162482e608f6bca4bd81d60500691f71
Elmira Ahmedova <88899832+eahmedova@users.noreply.github.com>
2023-02-10 11:57:26 +0100

    normalised json response to a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+# take the kson version of API response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output the normalized response as a table in streamlit
+streamlit.dataframe(fruityvice_normalized)
